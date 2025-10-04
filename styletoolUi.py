4b2b18eb0c1b3bd9e7033fcd6a7d58c7acf5a3b3
sun <archbcc37381@gmail.com>
2025-10-04 17:15:08 +0700

    style

---
+++ styletoolUi.py
@@ -0,0 +1,93 @@
+try:
+    from PySide6 import QtCore, QtGui, QtWidgets
+    from shiboken6 import wrapInstance
+except:
+    from PySide2 import QtCore, QtGui, QtWidgets
+    from shiboken2 import wrapInstance
+
+IMAGE_DIR = 'C:/Users/ICT68/Documents/maya/2025/scripts/styletool/image'
+import maya.OpenMayaUI as omui
+
+class StyleToolDialog(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Style Tool")
+        self.resize(300, 300)
+
+        self.mainLayout = QtWidgets.QVBoxLayout()
+        self.setLayout(self.mainLayout)
+        self.setStyleSheet(
+            '''
+            QDialog {
+                background-color:#D14dF4;
+            }
+            '''
+        )
+
+        self.imageLabel = QtWidgets.QLabel()
+        self.imagePixmap = QtGui.QPixmap(f'{IMAGE_DIR}/Untitled.png')
+        scaledPixmap = self.imagePixmap.scaled(
+            QtCore.QSize(64, 64),
+            QtCore.Qt.KeepAspectRatio,
+            QtCore.Qt.SmoothTransformation
+        )
+
+        self.imageLabel.setPixmap(scaledPixmap)
+        self.imageLabel.setAlignment(QtCore.Qt.AlignCenter)
+
+        self.mainLayout.addWidget(self.imageLabel)
+
+        self.nameLayout = QtWidgets.QVBoxLayout()
+        self.mainLayout.addLayout(self.nameLayout)
+        self.nameLabel = QtWidgets.QLabel("Name")
+        self.nameLineEdit = QtWidgets.QLineEdit()
+        self.nameLayout.addWidget(self.nameLabel)
+        self.nameLayout.addWidget(self.nameLineEdit)
+        self.nameLineEdit.setStyleSheet(
+            '''
+            QLineEdit {
+                background-color:qlineargradient(x1:0,y1:0.5,x2:0.5,y2:1,stop:0 blue,stop:1 navy);
+                color:white;
+                border-radius: 8px;
+                font-family:Arial;
+                font-weight:bold;
+            }
+            '''
+        )
+
+        self.buttonLayout = QtWidgets.QHBoxLayout()
+        self.mainLayout.addLayout(self.buttonLayout)
+        self.selectButton = QtWidgets.QPushButton("Select")
+        self.saveButton = QtWidgets.QPushButton("Cancel")
+        self.buttonLayout.addWidget(self.selectButton)
+        self.buttonLayout.addWidget(self.saveButton)
+
+        self.selectButton.setStyleSheet(
+            '''
+            QPushButton{
+                background-color: #6CAF40;
+                border-radius: 12px;
+                font-size:16px;
+                font-family:Papyrus;
+                font-weight:bold;
+                padding:4px;
+            }
+            QPushButton:hover{
+                background-color: qlineargradient(x1:0,y1:0,x2:0,y2:1,stop:0 red,stop:1 blue);
+            }
+            QPushButton:pressed{
+                background-color: black;
+            }
+            '''
+        )
+
+def run():
+    global ui
+    try:
+        ui.close()
+    except:
+        pass
+
+    ptr = wrapInstance(int(omui.MQtUtil.mainWindow()), QtWidgets.QWidget)
+    ui = StyleToolDialog(parent=ptr)
+    ui.show()
